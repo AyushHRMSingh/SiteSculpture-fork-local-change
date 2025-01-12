a60182c524976a13e639c89dcdf28cf91b565b62
Shivam Badhopulu <shivambadhopulu@gmail.com>
2025-01-12 13:54:36 +0530

    some frontend fixed floating fileexploere and some more changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Builder } from './pages/Builder';
+import { Builder } from './pages/builder';
 import { parseXml } from './steps';
 
 function App() {
